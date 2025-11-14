4f5aa9eccb259055bd23549c56a6c44ae570bbbb
Dmytro Yukhymuk <dima.yukhimuk@gmail.com>
2025-11-14 19:05:38 +0100

    Test added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
+import Test from '@/components/Test';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
-
-import LandingPage from '@/components/LandingPage';
 
 export default async function HomePage() {
 	const user = await currentUser();
 
-	if (!user) return <LandingPage />;
+	if (!user) return <Test />;
 
 	return redirect('/events');
 }
